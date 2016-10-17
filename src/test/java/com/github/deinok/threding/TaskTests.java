ae4431fa5f4828ceafcb6f640663064c4777c8cb
deinok <raul.hidalgo.sopa@gmail.com>
2016-10-17 10:42:21 +0200

    Tests

--- src/test/java/com/github/deinok/threding/TaskTests.java
+++ src/test/java/com/github/deinok/threding/TaskTests.java
@@ -2,29 +2,40 @@ package com.github.deinok.threding;
 
 import com.github.deinok.threading.Task;
 import com.github.deinok.threading.TaskRunnable;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TaskTests {
 
-      @Test(timeout = 10000)
-      public void sleepTest(){
-          Task<Void> task = new Task<Void>(new TaskRunnable<Void>() {
-              @Override
-              public Void execute() {
-                  try {
-                      Thread.sleep(5000);
-                  } catch (InterruptedException e) {
-                      throw new RuntimeException(e);
-                  }
-                  return null;
-              }
-          }).executeAsync();
+    @Test(timeout = 500)
+    public void sleepTest(){
+        Task<Void> task = new Task<Void>(new TaskRunnable<Void>() {
+            @Override
+            public Void execute() {
+                try {
+                    Thread.sleep(250);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                return null;
+                }
+        }).executeAsync();
 
-          try {
-              Thread.sleep(5000);
-          } catch (InterruptedException e) {
-              throw new RuntimeException(e);
-          }
-          task.await();
-      }
+        try {
+            Thread.sleep(250);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        task.await();
+    }
+    @Test
+    public void resultIntegerTest(){
+        Task<Integer> task = new Task<Integer>(new TaskRunnable<Integer>() {
+            @Override
+            public Integer execute() {
+                return 1;
+            }
+        }).executeAsync();
+        Assert.assertEquals(1, (int)task.getResult());
+    }
 }
