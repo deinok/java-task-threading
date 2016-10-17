ae48d56f1803a26b67969a5ee558519aa8c23b3a
deinok <raul.hidalgo.sopa@gmail.com>
2016-10-17 09:45:16 +0200

    Tests

--- src/test/java/com/github/deinok/threding/TaskTests.java
+++ src/test/java/com/github/deinok/threding/TaskTests.java
@@ -1,5 +1,30 @@
 package com.github.deinok.threding;
 
+import com.github.deinok.threading.Task;
+import com.github.deinok.threading.TaskRunnable;
+import org.junit.Test;
+
 public class TaskTests {
 
+      @Test(timeout = 10000)
+      public void sleepTest(){
+          Task<Void> task = new Task<Void>(new TaskRunnable<Void>() {
+              @Override
+              public Void execute() {
+                  try {
+                      Thread.sleep(5000);
+                  } catch (InterruptedException e) {
+                      throw new RuntimeException(e);
+                  }
+                  return null;
+              }
+          }).executeAsync();
+
+          try {
+              Thread.sleep(5000);
+          } catch (InterruptedException e) {
+              throw new RuntimeException(e);
+          }
+          task.await();
+      }
 }
