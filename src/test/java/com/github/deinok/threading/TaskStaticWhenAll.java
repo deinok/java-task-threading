b8ba35c2430b72ac311001323629fc6ac52d3fa1
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2017-10-22 13:46:05 +0200

    Task

--- src/test/java/com/github/deinok/threading/TaskStaticWhenAll.java
+++ src/test/java/com/github/deinok/threading/TaskStaticWhenAll.java
@@ -6,11 +6,11 @@ public class TaskStaticWhenAll {
 
 	@Test(timeout = 1500)
 	public void whenAllFact() {
-		Task<Void> task1 = Task.delay(1000).executeAsync();
-		Task<Void> task2 = Task.delay(1000).executeAsync();
-		Task<Void> task3 = Task.delay(1000).executeAsync();
+		Task task1 = Task.delay(1000).executeAsync();
+		Task task2 = Task.delay(1000).executeAsync();
+		Task task3 = Task.delay(1000).executeAsync();
 		Task[] taskArray = new Task[]{task1, task2, task3};
-		Task<Void> whenAllTask = Task.whenAll(taskArray);
+		Task whenAllTask = Task.whenAll(taskArray);
 		whenAllTask.await();
 	}
 
